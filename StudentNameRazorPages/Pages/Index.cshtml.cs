42c48bd2a66b01c0253a74416da84e9f7d790cbd
truongtnhe170291 <truongtnhe170291@fpt.edu.vn>
2025-03-02 20:21:49 +0700

    update

--- StudentNameRazorPages/Pages/Index.cshtml.cs
+++ StudentNameRazorPages/Pages/Index.cshtml.cs
@@ -3,27 +3,24 @@ using DataAccess.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Service;
-using System.Net;
-using System.Numerics;
 
 namespace StudentNameRazorPages.Pages
 {
 	public class IndexModel : PageModel
 	{
-		private readonly ILogger<IndexModel> _logger;
-		private readonly Ass2SignalRRazorPagesContext context;
 		private readonly ICustomerService customerService;
 
-		public IndexModel(ILogger<IndexModel> logger, Ass2SignalRRazorPagesContext context, ICustomerService customerService)
+		public IndexModel(ICustomerService customerService)
 		{
-			_logger = logger;
-			this.context = context;
 			this.customerService = customerService;
 		}
 		public List<Customer> Customers { get; set; }
+		[BindProperty(SupportsGet = true)]
+		[Range(0, 60, ErrorMessage = "Discount percentage must be between 0 - 60.")]
+		public int DiscountRate { get; set; }
 		public void OnGet()
 		{
-			Customers = context.Customers.ToList();
+			Customers = customerService.GetCustomersWithDiscount(DiscountRate);
 		}
 
 		public async Task<IActionResult> OnPost(IFormFile file)
@@ -84,27 +81,5 @@ namespace StudentNameRazorPages.Pages
 			return RedirectToPage();
 		}
 	}
-        public IndexModel(ILogger<IndexModel> logger, Ass2SignalRRazorPagesContext context, ICustomerService customerService)
-        {
-            _logger = logger;
-            this.context = context;
-            this.customerService = customerService;
-        }
-
-        [BindProperty]
-        public List<Customer> Customers { get; set; }
-        [BindProperty(SupportsGet = true)]
-        [Range(0, 60, ErrorMessage = "Discount percentage must be between 0 - 60.")]
-        public int DiscountRate { get; set; }
-        public void OnGet()
-        {
-            //Console.WriteLine(DiscountRate);
-            //DiscountRate = 30;
-            //if(DiscountRate <=0 || DiscountRate > 60)
-            //{
-            //    ViewData["ErrorSearch"] = "a";
-            //}
-            Customers = customerService.GetCustomersWithDiscount(DiscountRate);
-        }
-    }
+        
 }
