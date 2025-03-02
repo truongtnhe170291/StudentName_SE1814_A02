b887cc54eb79c7862ee01b171a533329212a6c72
Nguyen Huy Hoang <hoangnh@hexigon.tech>
2025-03-02 16:07:33 +0700

    search user and validate

--- StudentNameRazorPages/Pages/Index.cshtml.cs
+++ StudentNameRazorPages/Pages/Index.cshtml.cs
@@ -1,6 +1,8 @@
+﻿using System.ComponentModel.DataAnnotations;
 using DataAccess.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Service;
 
 namespace StudentNameRazorPages.Pages
 {
@@ -8,16 +10,29 @@ namespace StudentNameRazorPages.Pages
     {
         private readonly ILogger<IndexModel> _logger;
         private readonly Ass2SignalRRazorPagesContext context;
+        private readonly ICustomerService customerService;
 
-        public IndexModel(ILogger<IndexModel> logger, Ass2SignalRRazorPagesContext context)
+        public IndexModel(ILogger<IndexModel> logger, Ass2SignalRRazorPagesContext context, ICustomerService customerService)
         {
             _logger = logger;
             this.context = context;
+            this.customerService = customerService;
         }
+
+        [BindProperty]
         public List<Customer> Customers { get; set; }
+        [BindProperty(SupportsGet = true)]
+        [Range(0, 60, ErrorMessage = "Discount percentage must be between 0 - 60.")]
+        public int DiscountRate { get; set; }
         public void OnGet()
         {
-            Customers = context.Customers.ToList();
+            //Console.WriteLine(DiscountRate);
+            //DiscountRate = 30;
+            //if(DiscountRate <=0 || DiscountRate > 60)
+            //{
+            //    ViewData["ErrorSearch"] = "a";
+            //}
+            Customers = customerService.GetCustomersWithDiscount(DiscountRate);
         }
     }
 }
